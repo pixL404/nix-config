9dd760614041b93a27d67ef9b934d4459b67db77
alex <alexostresso@gmail.com>
2023-10-26 22:20:15 +0200

    use catppuccin theme for foot terminal

--- home/foot.nix
+++ home/foot.nix
@@ -1,9 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  catppuccinColors = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/foot/4e5b4a78dec83b1778e01556f15cabd9c8c9ed4f/catppuccin-frappe.conf";
+    hash = "sha256-5KjNdz4jpRzvB/UJjcMRBJYbTy4JHlUcfsiewo4ANME=";
+  };
+in
 {
   programs.foot = {
     enable = true;
     server.enable = true;
-    settings = {
+  };
+
+  xdg.configFile."foot/foot.ini".text = lib.generators.toINIWithGlobalSection {} {
+    globalSection = {
+      include = catppuccinColors;
+    };
+    sections = {
       main = {
         term = "foot";
 
@@ -35,8 +47,9 @@
       };
 
       colors = {
-        alpha = "0.8";
+        alpha = 0.8;
       };
+
     };
   };
 }
