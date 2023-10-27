b556dc0c1a9706c3486e63200e0d5456e123bba0
alex <alexostresso@gmail.com>
2023-10-27 22:09:05 +0200

    push theme.nix

---
+++ home/theme.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }:
+{
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Catpuccin-Frappe-Standard-Mauve-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        accents = [ "mauve" ];
+        size = "standard";
+        variant = "frappe";
+      };
+    };
+    cursorTheme = {
+      package = pkgs.catppuccin-cursors.frappeDark;
+      name = "Catpuccin-Frappe-Dark";
+      size = 32;
+    };
+  };
+  
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.catppuccin-cursors.frappeDark;
+    name = "Catpuccin-Frappe-Dark";
+    size = 32;
+  };
+}
