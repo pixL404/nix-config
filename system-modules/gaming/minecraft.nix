137f13252d8775710b99c0f7f2929b4d130db6cc
alex <alexostresso@gmail.com>
2024-02-10 20:45:57 +0100

    add modded minecraft server

--- system-modules/gaming/minecraft.nix
+++ system-modules/gaming/minecraft.nix
@@ -4,18 +4,18 @@
   ...
 }:
 let
-  minecraft-server-1-19-2 = pkgs.minecraftServers.vanilla-1-19.overrideAttrs (old: {
-    version = "1.19.2";
-    src = pkgs.fetchurl {
-      url = "https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar";
-      sha1 = "f69c284232d7c7580bd89a5a4931c3581eae1378";
-    };
-  });
   minecraft-server-1-20-1 = pkgs.minecraftServers.vanilla-1-20.overrideAttrs (old: {
     version = "1.20.1";
     src = pkgs.fetchurl {
       url = "https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar";
       sha1 = "84194a2f286ef7c14ed7ce0090dba59902951553";
+    };
+  });
+  fabric-server-1-20-1 = pkgs.minecraftServers.vanilla-1-20.overrideAttrs (old: {
+    version = "fabric-1.20.1";
+    src = pkgs.fetchurl {
+      url = "https://meta.fabricmc.net/v2/versions/loader/1.20.1/0.15.6/1.0.0/server/jar";
+      hash = "sha256-vOULXIe2hz5p49rdlXINsHYsl5whx6PQE0HnLxd6Vl0=";
     };
   });
 in
@@ -28,36 +28,70 @@ in
 
   # TODO: enable this on self-defined flag
   networking.firewall.allowedTCPPorts = [ 25565 ];
-  containers.minecraft-server = {
-    autoStart = false;
-    privateNetwork = false;
+  containers = {
+    minecraft-server = {
+      autoStart = false;
+      privateNetwork = false;
 
-    # so the server doesn't stop if I change my config
-    restartIfChanged = false;
-    timeoutStartSec = "5min";
-    forwardPorts = {
-      containerPort = 25565;
-      hostPort = 25565;
-      protocol = "tcp";
-    };
+      # so the server doesn't stop if I change my config
+      restartIfChanged = false;
+      timeoutStartSec = "5min";
+      forwardPorts = {
+        containerPort = 25565;
+        hostPort = 25565;
+        protocol = "tcp";
+      };
 
-    bindMounts = {
-      "/data" = {
-        hostPath = "/data/minecraft/server/";
-        isReadOnly = false;
+      bindMounts = {
+        "/data" = {
+          hostPath = "/data/minecraft/server/";
+          isReadOnly = false;
+        };
+      };
+      
+      config = {
+        services.minecraft-server = {
+          enable = true;
+          eula = true;
+          package = minecraft-server-1-20-1;
+          jvmOpts = "-Xms10G -Xmx12G -XX:+UseG1GC";
+          dataDir = "/data";
+          declarative = false;
+        };
+      system.stateVersion = "24.05";
       };
     };
-    
-    config = {
-      services.minecraft-server = {
-        enable = true;
-        eula = true;
-        package = minecraft-server-1-20-1;
-        jvmOpts = "-Xms10G -Xmx12G -XX:+UseG1GC";
-        dataDir = "/data";
-        declarative = false;
+    fabric-server = {
+      autoStart = false;
+      privateNetwork = false;
+
+      # so the server doesn't stop if I change my config
+      restartIfChanged = false;
+      timeoutStartSec = "5min";
+      forwardPorts = {
+        containerPort = 25565;
+        hostPort = 25565;
+        protocol = "tcp";
       };
-    system.stateVersion = "24.05";
+
+      bindMounts = {
+        "/data" = {
+          hostPath = "/data/minecraft/fabric-server/";
+          isReadOnly = false;
+        };
+      };
+      
+      config = {
+        services.minecraft-server = {
+          enable = true;
+          eula = true;
+          package = fabric-server-1-20-1;
+          jvmOpts = "-Xms10G -Xmx12G -XX:+UseG1GC";
+          dataDir = "/data";
+          declarative = false;
+        };
+      system.stateVersion = "24.05";
+      };
     };
   };
 }
