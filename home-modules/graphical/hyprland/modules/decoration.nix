8be1a3b70f1ff5ed45c0eaaaa485b2ea56f3841e
alex <alexostresso@gmail.com>
2024-12-05 19:44:58 +0100

    update flake lock

--- home-modules/graphical/hyprland/modules/decoration.nix
+++ home-modules/graphical/hyprland/modules/decoration.nix
@@ -10,8 +10,9 @@
     passes = 1;
   };
 
-  drop_shadow = true;
-  shadow_range = 4;
-  shadow_render_power = 3;
-  # "col.shadow" = "rgba(1a1a1aee)";
+  shadow = {
+    enabled = true;
+    range = 4;
+    render_power = 3;
+  };
 }
